61174f8d6c2517defc7ecbde98e0a91e8d9c0ab8
Dinh Vuong <37194490+vpdinh@users.noreply.github.com>
2019-01-03 10:01:34 -0800

    Update cryptolive.js

--- cryptolive.js
+++ cryptolive.js
@@ -206,7 +206,7 @@ function optionChanged2() {
 
 //BITCOIN NEWS
 
-setInterval(function(){
+//setInterval(function(){
 $.get("https://api.mlab.com/api/1/databases/cryptocurrency/collections/cryptonews?apiKey=BiFVfOzkzS0l9zD2toWP8k6ZCZ6JJUK6", function(data, status){
   console.log(data);
   var source = [];
@@ -228,7 +228,7 @@ $.get("https://api.mlab.com/api/1/databases/cryptocurrency/collections/cryptonew
     .on("click", function(d){
       window.open(d.link)});
 });
-},5000);
+//},5000);
 
 
 
